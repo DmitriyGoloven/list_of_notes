acb2fc31070767022f9710c822614fb5da5a5091
DmitriyHoloven <goloven.dmitriy@gmail.com>
2022-07-30 16:25:20 +0300

    copy

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import {v4 as uuidv4} from 'uuid';
 
 function App() {
 
-    const [note, setNote] = useState("")
+    const [note, setNote] = useState(" ")
     const [notes, setNotes] = useState(
-        localStorage.getItem("notes") || []
+        JSON.parse(localStorage.getItem("notes")) || []
     )
 
     useEffect(() => {
-
             localStorage.setItem('notes', JSON.stringify(notes))
         },
         [notes])
@@ -27,31 +26,33 @@ function App() {
         } else alert('Note is empty!')
     }
     const delNote = (id) => {
-        console.log(id)
-        console.log(notes.filter((el) => el.id !== id))
         setNotes(notes.filter((el) => el.id !== id))
     }
+
+
+    const listItem = notes.map((note, index) => {
+        return <>
+            <li style={{margin: "5px 0 "}} key={note.id}>
+                <textarea className={"myNote"} defaultValue={note.note}/>
+                <button className={"noteButton"} onClick={() => delNote(note.id)}>del</button>
+                <button className={index !== 0 ? "noteButton" : "none"}>up</button>
+                <button className={index === notes.length - 1 ? "none" : "noteButton"}>down</button>
+            </li>
+        </>
+
+    })
+
+
+
 
     return (
         <div className={"app"}>
             <div className={"board"}>
 
                 <ul>
+                    {listItem}
 
-                    {notes.map((note, index) => {
-                        return <>
-                            <li style={{margin: "5px 0 "}}>
-                                <textarea className={"myNote"} defaultValue={note.note}/>
-                                <button className={"noteButton"} onClick={() => delNote(note.id)}>del</button>
-                                <button className={index !== 0 ? "noteButton" : "none"}>up</button>
-                                <button className={index === notes.length - 1 ? "none" : "noteButton"}>down</button>
-                            </li>
-                        </>
-
-                    })}
-
-
-                    <li><input className={"noteInput"}
+                    <li key={'add'}><input className={"noteInput"}
                                type={"text"}
                                value={note}
                                placeholder={"Your note"}
